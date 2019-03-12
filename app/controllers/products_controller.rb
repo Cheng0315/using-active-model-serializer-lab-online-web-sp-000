778da295b5e110b0dac1ca8f58a5904deabdb26d
Cheng Vue <csalvue@gmail.com>
2019-03-12 06:48:39 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,7 +25,6 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
     respond_to do |format|
-      format.html { render :show }
       format.json { render json: @product}
     end
   end
